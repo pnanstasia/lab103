5fc0a9c31c705c8dcf63416e4c5528cdfa174607
pnanstasia <kucheruk.pn@ucu.edu.ua>
2023-11-27 18:59:48 +0200

    update file

--- src/main/java/ua/edu/ucu/apps/CachedDocument.java
+++ src/main/java/ua/edu/ucu/apps/CachedDocument.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+
 public class CachedDocument {
     private static Connection connection;
 
@@ -20,28 +21,43 @@ public class CachedDocument {
         String url = "jdbc:sqlite:data.db";
         Connection conn = null;
         try {
+            Class.forName("org.sqlite.JDBC");
             conn = DriverManager.getConnection(url);
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             System.out.println(e.getMessage());
         }
         return conn;
     }
 
     public boolean recordExist(String gcsPath) {
-       String sql = "SELECT count(*) FROM your table WHERE path = ?";
-       try (Connection conn = this.connect();
-            PreparedStatement preparedStatement = conn.prepareStatement(sql)){
-                preparedStatement.setString(1, gcsPath);
-                ResultSet rs = preparedStatement.executeQuery();
-                return rs.getInt(1) > 0;
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-            }
-            return false;
+        String sql = "SELECT count(*) FROM documents WHERE path = ?";
+        try (Connection conn = this.connect();
+             PreparedStatement preparedStatement = conn.prepareStatement(sql)){
+            preparedStatement.setString(1, gcsPath);
+            ResultSet rs = preparedStatement.executeQuery();
+            return rs.getInt(1) > 0;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public void createTable() {
+        String createtable = "CREATE TABLE IF NOT EXISTS documents ("
+                        + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                        + "path TEXT NOT NULL,"
+                        + "document TEXT NOT NULL);";
+        try (Connection conn = this.connect();
+             PreparedStatement prepared = conn.prepareStatement(createtable)) {
+            prepared.executeUpdate();
+            System.out.println("Database creation");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void injectText(String text, String gcsPath) {
-        String sql = "SELECT count(*) FROM your table WHERE path = ?";
+        String sql = "INSERT INTO documents (path, document) VALUES (?, ?)";
         try (Connection con = this.connect();
             PreparedStatement pst = con.prepareStatement(sql)) {
                 pst.setString(1, gcsPath);
@@ -54,8 +70,9 @@ public class CachedDocument {
 
     public static void main(String[] args) {
         CachedDocument app = new CachedDocument();
-        String gcsPath = "path";
-        
+        app.connect();
+        app.createTable();
+        String gcsPath = "data.db";
         if (app.recordExist(gcsPath)) {
             app.injectText("text, which i want find", gcsPath);
             System.out.println("Record updated");
